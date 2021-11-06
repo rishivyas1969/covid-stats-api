20de7b92ee7f012484290b78541d345432989cf2
rishi1969 <rishivyas2424@gmail.com>
2021-11-07 00:38:48 +0530

    hide keys

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Console } from 'console';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IndiaModule } from './india/india.module';
+require('dotenv').config();
 
 @Module({
   imports: [IndiaModule,
-  MongooseModule.forRoot('mongodb+srv://nigga1969:euYOQIu2vewfNTv5@india-covid-data.zil1e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
+  MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
